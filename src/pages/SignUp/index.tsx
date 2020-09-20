5cc042a4310c6451e579fb936bcb3cd6e1fd572f
andre.mulin <aesmulin@gmail.com>
2020-09-20 19:22:30 -0300

    chapter02-class05

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,73 @@
+import React, { useRef } from 'react';
+import {
+	Image,
+	View,
+	ScrollView,
+	KeyboardAvoidingView,
+	Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+	Container,
+	Title,
+	BackToSignInButton,
+	BackToSignInButtonText,
+} from './styles';
+
+import logoImg from '../../assets/logo.png';
+
+const SignUp: React.FC = () => {
+	const formRef = useRef<FormHandles>(null);
+	const navigation = useNavigation();
+
+	return (
+		<>
+			<KeyboardAvoidingView
+				style={{ flex: 1 }}
+				behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+				enabled
+			>
+				<ScrollView
+					keyboardShouldPersistTaps="handled"
+					contentContainerStyle={{ flex: 1 }}
+				>
+					<Container>
+						<Image source={logoImg} />
+						<View>
+							<Title>Crie sua conta</Title>
+						</View>
+
+						<Form
+							ref={formRef}
+							onSubmit={data => {
+								console.log(data);
+							}}
+						>
+							<Input name="name" icon="user" placeholder="Nome" />
+							<Input name="email" icon="mail" placeholder="E-mail" />
+							<Input name="password" icon="lock" placeholder="Senha" />
+
+							<Button onPress={() => formRef.current?.submitForm()}>
+								Cadastrar
+							</Button>
+						</Form>
+					</Container>
+
+					<BackToSignInButton onPress={() => navigation.goBack()}>
+						<Icon name="arrow-left" size={20} color="#f4ede8" />
+						<BackToSignInButtonText>Voltar para o login</BackToSignInButtonText>
+					</BackToSignInButton>
+				</ScrollView>
+			</KeyboardAvoidingView>
+		</>
+	);
+};
+
+export default SignUp;
